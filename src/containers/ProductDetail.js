992febd98aacf5032c080e514d6a91ecfe17d920
Liyakat Shaikh <liyakatshaikh07@gmail.com>
2021-06-07 18:03:30 +0530

    till selectedProducts reducer

---
+++ src/containers/ProductDetail.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectedProduct } from '../redux/actions/productActions';
+
+const ProductDetail = () => {
+    const product = useSelector(state=> state.product);
+    const { productId } = useParams();
+    const dispatch = useDispatch();
+    console.log(productId);
+    const fetchProductDetail = async() => {
+        const response = await axios.get(`https://fakestoreapi.com/products/${productId}`).catch(err => {
+            console.log("Error", err);  
+        });
+        dispatch(selectedProduct(response.data));
+    }
+    return (
+        <div>
+            <h1> Product Detail </h1>
+        </div>
+    )
+}
+
+export default ProductDetail
