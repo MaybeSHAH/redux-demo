d2e9c21f79013fb82e537e00b22c92d256d7bd10
Liyakat Shaikh <liyakatshaikh07@gmail.com>
2021-06-08 17:16:22 +0530

    redux integrated

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -21,3 +21,13 @@ export const selectedProduct = (product) => {
     
 
 };
+
+export const removeSelectedProduct = () => {
+
+    return {
+        type: ActionTypes.REMOVE_SELECTED_PRODUCT
+
+        };
+    
+
+};
